6fc32003292d7019816ca67090a8c9839c501adb
Codeur974 <eric.sermande@gmail.com>
2025-04-19 21:52:03 +0400

    Résolution des conflits dans vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: "/Portfolio/", // Chemin de base pour GitHub Pages
-=======
-  base: "/Portfolio/",
->>>>>>> 3269212e35185ad3b55079673dff85d10dedde8a
   plugins: [react()],
 });
