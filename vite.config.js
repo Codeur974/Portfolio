16fe856cde895ad5b33eeab636712ee9995aa92b
Codeur974 <eric.sermande@gmail.com>
2025-05-09 21:15:19 +0400

    Correction des dépendances et configuration Vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/", // Chemin de base pour GitHub Pages
+  base: "/", // Chemin de base pour GitHub Pages
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Remplacez "some-package" par le nom de la dépendance
+    },
+  },
 });
